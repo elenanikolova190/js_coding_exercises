5eae2d95228ffe218e3f18061f1173273202133d
Elena Nikolova <elena.nikolova190@gmail.com>
2020-09-14 14:39:35 +0100

    Completed all functions from exercise004 wusing map and filter

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,10 +1,11 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   
-  var arrOut = []
-  nums.forEach(function(item) {
+  var arrOut = nums.filter(function(item) {
     if(item < 1){
-      arrOut.push(item)
+      return true
+    } else {
+      return false 
     }
   })
  return arrOut
@@ -14,11 +15,12 @@ function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   
-  var namesOut = []
-  names.forEach(function(item){
+  var namesOut = names.filter(function(item){
     if( item[0] == char){
-      namesOut.push(item)
-    } 
+      return true
+    } else{
+      return false
+    }
   })
   return namesOut
 }
@@ -26,10 +28,11 @@ function findNamesBeginningWith(names, char) {
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   
-  var wordsOut = []
-  words.forEach(function(item){
+  var wordsOut = words.filter(function(item){
     if(item.substring(0,3) == "to "){
-      wordsOut.push(item)
+      return true
+    } else{
+      return false
     }
   })
 return wordsOut
@@ -38,10 +41,11 @@ return wordsOut
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
 
-  var arrOut = []
-  nums.forEach(function(item) {
+  var arrOut = nums.filter(function(item) {
     if(item % 1 === 0){
-      arrOut.push(item)
+      return true
+    } else{
+      return false
     }
   })
 return arrOut  
@@ -60,9 +64,8 @@ return cities
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   
-  var numsOut = []
-  nums.forEach(function(item) {
-    numsOut.push(Math.round(Math.sqrt(item) * 100) / 100 )
+  var numsOut = nums.map(function(item) {
+    return Math.round(Math.sqrt(item) * 100) / 100 
   });
   return numsOut
 }
@@ -71,10 +74,11 @@ function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   
-  var strOut = [];
-  sentences.forEach(function(item) {
+  var strOut = sentences.filter(function(item) {
     if(item.toLowerCase().indexOf(str.toLowerCase()) != -1){
-      strOut.push(item)
+      return true
+    } else {
+      return false
     }
   })
   return strOut
@@ -83,9 +87,8 @@ function findSentencesContaining(sentences, str) {
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   
-  var arrOut = []
-  triangles.forEach(function(item) {
-    arrOut.push( Math.max(...item))
+  var arrOut = triangles.map(function(item) {
+    return Math.max(...item)
   });
   return arrOut
 }
